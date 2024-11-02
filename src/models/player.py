1b75f2dcf5662b48ad278ea8cf92f67db883a3db
WasinUddy <wasinsilakong@gmail.com>
2024-11-02 14:42:10 +0700

    Move RadarScreen to be part of Player Model

--- src/models/player.py
+++ src/models/player.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, conlist, field_validator
 class Player(BaseModel):
     name: str
     ships: conlist(int, min_length=16, max_length=16)
+    radar_screen: conlist(int, min_length=64, max_length=64) = [0] * 64
 
     @field_validator('ships')
     def validate_state(cls, v):
@@ -11,5 +12,13 @@ class Player(BaseModel):
             raise ValueError('ship config must contain unique values')
         if any(value < 0 or value > 63 for value in v):
             raise ValueError('ship config values must be in the range 0-63')
+        return v
+
+    @field_validator('radar_screen', check_fields=False)
+    def validate_screen(cls, v):
+        if any(value < -1 or value > 1 for value in v):
+            raise ValueError('game rendering value must be in the range -1 to 1')
+        if v.count(1) > 16:
+            raise ValueError(f'Too many ones in the game rendering; expected <= 16, got {v.count(1)}')
         return v
     
